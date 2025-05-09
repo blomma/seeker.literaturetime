5412d850a2ec46c3947d31401c6d1ac9e93b0131
Mikael Hultgren <blomma@artsoftheinsane.com>
2025-05-09 02:35:22 +0200

    required

--- src/Models/CatalogEntry.cs
+++ src/Models/CatalogEntry.cs
@@ -4,34 +4,32 @@ namespace seeker.literaturetime.models;
 
 // Text#,Type,Issued,Title,Language,Authors,Subjects,LoCC,Bookshelves
 
-#pragma warning disable CA1812
 internal sealed class CatalogEntry
 {
     [Name("Text#")]
-    public string GutenbergReference { get; set; } = string.Empty;
+    public required string GutenbergReference { get; set; }
 
     [Name("Type")]
-    public string Type { get; set; } = string.Empty;
+    public required string Type { get; set; }
 
     [Name("Issued")]
-    public string Issued { get; set; } = string.Empty;
+    public required string Issued { get; set; }
 
     [Name("Title")]
-    public string Title { get; set; } = string.Empty;
+    public required string Title { get; set; }
 
     [Name("Language")]
-    public string Language { get; set; } = string.Empty;
+    public required string Language { get; set; }
 
     [Name("Authors")]
-    public string Authors { get; set; } = string.Empty;
+    public required string Authors { get; set; }
 
     [Name("Subjects")]
-    public string Subjects { get; set; } = string.Empty;
+    public required string Subjects { get; set; }
 
     [Name("LoCC")]
-    public string LoCC { get; set; } = string.Empty;
+    public required string LoCC { get; set; }
 
     [Name("Bookshelves")]
-    public string Bookshelves { get; set; } = string.Empty;
+    public required string Bookshelves { get; set; }
 }
-#pragma warning restore CA1812
